bc8d818b2555699b24384e403365044b81bbe97c
raghad497 <raghadraslan30@hotmail.com>
2021-07-06 17:02:31 +0300

    new content

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 'use strict';
 let firstname = prompt('Welcome to my guessing game ! write your first name please :');
 alert('Welcome ' + firstname + ' nice to meet you , let\'s start guessing !');
+let score=0;
 
 let favcolor = prompt('Is My Favourite color pink ?');
 
 favcolor = favcolor.toLowerCase();
 
-if (favcolor === 'yes') {
-
+if (favcolor === 'yes'||favcolor === 'y') {
+    score++;
     alert('Excellent '+firstname+' yes it is Pink ! Such a beautiful color !');
 
 }
-else if (favcolor === 'no') {
+else if (favcolor === 'no'||favcolor === 'n') {
+
     alert('Sorry '+firstname+ ' wrong answer , pink was indeed my favourite !');
+
 }
-    
+
 
 else {
     alert('please write yes or no');
@@ -24,15 +27,17 @@ let favseason = prompt('Is My Favourite season summer ?');
 
 favseason = favseason.toLowerCase();
 
-if (favseason === 'yes') {
+if (favseason === 'yes'||favseason === 'y') {
 
     alert('Nop '+firstname+' I hate Summer !');
 
+
 }
-else if (favseason === 'no') {
+else if (favseason === 'no'||favseason === 'n') {score++;
     alert('correct answer '+firstname+' Summer is NOT my favourite season !');
+
 }
-    
+
 
 else {
     alert('please write yes or no');
@@ -42,15 +47,16 @@ let favfruit = prompt('Is My Favourite fruit Strawberries ?');
 
 favfruit = favfruit.toLowerCase();
 
-if (favfruit === 'yes') {
+if (favfruit === 'yes'||favfruit === 'y') {score++;
 
     alert('You are correct '+firstname+' Strawberries are my favourites , So delicious !');
 
 }
-else if (favfruit === 'no') {
+else if (favfruit === 'no'||favfruit === 'n') {
     alert('Sorry '+firstname+' wrong answer !');
+
 }
-    
+
 
 else {
     alert('please write yes or no');
@@ -61,15 +67,16 @@ let favdrink = prompt('Is My Favourite drink Tea ?');
 
 favdrink = favdrink.toLowerCase();
 
-if (favdrink === 'yes') {
+if (favdrink === 'yes'||favdrink === 'y') {
 
     alert('I\'m sorry '+firstname+' My favourite drink is Coffee !');
 
 }
-else if (favdrink === 'no') {
+else if (favdrink === 'no'||favdrink === 'n') {score++;
     alert('Indeed '+firstname+' my favourite drink is definitely not tea !');
+
 }
-    
+
 
 else {
     alert('please write yes or no');
@@ -80,21 +87,29 @@ let favpet = prompt('Do you think My Favourite pet would be a Dog?');
 
 favpet = favpet.toLowerCase();
 
-if (favpet === 'yes') {
+if (favpet === 'yes'||favpet === 'y') {
 
     alert('Nop ! sorry '+firstname+' I\'m actually afraid of dogs !');
 
 }
-else if (favpet === 'no') {
+else if (favpet === 'no'||favpet === 'n') {score++;
     alert('correct answer '+firstname+' my favourite pet would be a cat ! they are adorable !');
+
 }
-    
+
 
 else {
     alert('please write yes or no');
 }
 
+alert('you have a score of '+ score+'/5');
+
 alert('thanks for playing '+firstname+' please click ok to continue to my website')
+
+
+
+
+
 
 
 
